d66c581a988b0b1853d13ad7df7ce7cf0fd2dfa0
Davi Moreira <62727795+DaviMoreira58@users.noreply.github.com>
2024-02-02 01:25:53 -0300

    Update script.js

--- Validador_CPF_CNPJ/script.js
+++ Validador_CPF_CNPJ/script.js
@@ -190,11 +190,15 @@ function validarCNPJ() {
     }
 }
 
+// Função para formatar o CNPJ no padrão XX.XXX.XXX/YYYY-ZZ
 function formatarCNPJ(input) {
     var cnpj = input.value.replace(/\D/g, "");
 
-    if (cnpj.length === 14) {
-        input.value = cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+    if (cnpj.length === 8) {
+        input.value = cnpj.replace(/(\d{2})(\d{3})(\d{3})$/, "$1.$2.$3/");
+    }
+    else if (cnpj.length === 14) {
+            input.value = cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
     } else {
         input.value = cnpj;
     }
